da72aa5f93e373a2d94e89ddfef3e9740afacd7e
DiegoG0477 <diego.14.13.lopez@gmail.com>
2025-03-24 01:37:04 -0600

    refactor: improve entity detection process

--- app/runtime/main.py
+++ app/runtime/main.py
@@ -1,12 +1,11 @@
 import sys
 import os
+from bridge import python_adapter
+from core.data.services.entity_detection_service import configure_nlp
 
 # Agregar la ruta del proyecto a sys.path
 #PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..'))
 #sys.path.insert(0, PROJECT_ROOT)
-
-
-from bridge import python_adapter
 
 def main():
     # Inicializa el núcleo del backend, carga configuraciones, etc.
@@ -14,6 +13,8 @@ def main():
     
     # Ejecuta el adapter para recibir mensajes desde Electron
     python_adapter.run_adapter()
+
+    configure_nlp()
 
 if __name__ == '__main__':
     main()
